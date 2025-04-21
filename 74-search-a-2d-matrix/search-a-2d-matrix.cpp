a838a6efd5c3ccb5bfedbf5efba22b2e612a26c7
Parth Sohal <sohalparth72@gmail.com>
2025-04-21 05:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.73%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -10,26 +10,46 @@ public:
         //     }
         // }
         // return false;
-        int row = matrix.size()-1;
-        int col = matrix[0].size()-1;
 
-        for(int i = 0 ; i < matrix.size() ;i++){
-            if(matrix[i][0] <= target && matrix[i][col] >= target){
-                int low = 0;
-                int high = matrix[i].size()-1;
-                while(low <= high){
-                    int mid = (low+high)/2;
-                    if(matrix[i][mid] == target){
-                        return true;
-                    }
-                    else if(matrix[i][mid] > target){
-                        high = mid - 1;
-                    }
-                    else{
-                        low = mid + 1;
-                    }
-                }
-            }
+        // 2 way --
+        //  int row = matrix.size()-1;
+        //  int col = matrix[0].size()-1;
+
+        // for(int i = 0 ; i < matrix.size() ;i++){
+        //     if(matrix[i][0] <= target && matrix[i][col] >= target){
+        //         int low = 0;
+        //         int high = matrix[i].size()-1;
+        //         while(low <= high){
+        //             int mid = (low+high)/2;
+        //             if(matrix[i][mid] == target){
+        //                 return true;
+        //             }
+        //             else if(matrix[i][mid] > target){
+        //                 high = mid - 1;
+        //             }
+        //             else{
+        //                 low = mid + 1;
+        //             }
+        //         }
+        //     }
+        // }
+        // return false;
+
+        int col = matrix[0].size();
+        int start = 0;
+        int end = matrix.size() * matrix[0].size() - 1;
+        // cout << end;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int element = matrix[mid / col][mid % col];
+
+            if (element == target) {
+                return true;
+            } else if (element > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
         }
         return false;
     }
