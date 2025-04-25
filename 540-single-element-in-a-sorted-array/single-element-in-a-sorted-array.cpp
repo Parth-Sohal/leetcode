98ccb3b87b967b3b1a69cb55ffe3b1a22f3aa01b
Parth Sohal <sohalparth72@gmail.com>
2025-04-25 14:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.85%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -8,42 +8,35 @@ public:
         if(nums.size() == 1){
             return nums[0];
         }
-
         if (nums[0] != nums[1]) {
             return nums[0];
         }
         if (nums[nums.size() - 1] != nums[end]) {
             return nums[nums.size()-1];
         }
+        while(start < end){
+        int mid = (start + end)/2;
 
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            cout << nums[mid] << endl;
-
-            // if(arr[mid-1] != arr[mid] && arr[mid+1] != arr[mid]){
-            //     cout << arr[mid] << endl;
-            //     // break;
-            // }
-
-            if (mid % 2 != 0) // odd
-            {
-                if (nums[mid - 1] == nums[mid]) {
-                    start = mid + 1;
-                } else {
-                    end = mid;
-                }
+        if(mid%2 != 0){//odd
+            if(nums[mid-1] == nums[mid]){
+                start = mid + 1; // isse phele saab ok
             }
-
-            else { // even
-                if (nums[mid] == nums[mid + 1]) {
-                    start = mid + 2;
-                } else {
-                    end = mid;
-                }
+            else{
+                end = mid - 1;
             }
         }
+        else{//even
+            if(nums[mid] == nums[mid+1]){
+                start = mid + 2;
+            }
+            else{
+                end = mid ;
+            }
+        }
+    }
 
-        return nums[start];
+    return nums[start];
+
+
     }
 };
