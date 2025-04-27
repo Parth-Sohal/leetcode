08aeb8da2952b82adfd2e47ddcc19b6fa0b4b0fa
Parth Sohal <sohalparth72@gmail.com>
2025-04-28 01:21:00 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.61%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,13 +3,33 @@ public:
     int search(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size() - 1;
-        
-        for(int i = 0 ; i < nums.size() ;i++){
-            if(nums[i] == target){
-                return i;
+        // int target = 2;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            // cout << nums[mid] << endl;
+            // cout << nums[start] << " " << nums[end] << endl;
+
+            if (nums[mid] == target) {
+                return mid;
+                // cout << "Here" << mid << " " << nums[mid] << endl;
+                // break;
+            }
+
+            if (nums[mid] < nums[end]) {
+                if (target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
         return -1;
-        
     }
 };
