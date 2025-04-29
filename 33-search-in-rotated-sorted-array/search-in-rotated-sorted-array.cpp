a3ee70f10dd9bfcf4d73f46e79a08167705284e6
Parth Sohal <sohalparth72@gmail.com>
2025-04-30 00:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.67%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
-        // int target = 2;
+        
+        int start = 0 ;
+        int end = nums.size()-1;
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            // cout << nums[mid] << endl;
-            // cout << nums[start] << " " << nums[end] << endl;
-
-            if (nums[mid] == target) {
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(nums[mid] == target){
                 return mid;
-                // cout << "Here" << mid << " " << nums[mid] << endl;
-                // break;
             }
-
-            if (nums[mid] < nums[end]) {
-                if (target <= nums[end] && target > nums[mid]) {
+            if(nums[mid] < nums[end]){
+                if(target > nums[mid] && target <= nums[end]){
                     start = mid + 1;
-                } else {
+                }
+                else{
                     end = mid - 1;
                 }
-            } else {
-                if (target >= nums[start] && target < nums[mid]) {
+            }
+            else{
+                if(target < nums[mid] && target >= nums[start]){
                     end = mid - 1;
-                } else {
+                }
+                else{
                     start = mid + 1;
                 }
             }
