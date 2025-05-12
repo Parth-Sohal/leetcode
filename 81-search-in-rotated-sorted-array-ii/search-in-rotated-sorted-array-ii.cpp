781473eeb8e007cc427a1cc2561ed561e53db6d3
Parth Sohal <sohalparth72@gmail.com>
2025-05-12 20:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (55.39%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -4,35 +4,39 @@ public:
         int start = 0;
         int end = nums.size() - 1;
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] == target) {
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(nums[mid] == target){
                 return true;
             }
-
-           if(nums[start] == nums[mid]){
-                start++;
-                continue;
-           }
-           if(nums[end] == nums[mid]){
+            if(nums[mid] == nums[end] && mid!=end){
                 end--;
                 continue;
-           }
-
-            if (nums[mid] <= nums[end]) {
-                if (target <= nums[end] && target > nums[mid]) {
+            }
+            if(nums[start] == nums[mid] && start!=mid){
+                start++;
+                continue;
+            }
+            
+            if(nums[mid] < nums[end]){
+                if(target > nums[mid] && target <= nums[end]){
                     start = mid + 1;
-                } else {
+                }
+                else{
                     end = mid - 1;
                 }
-            } else {
-                if (target >= nums[start] && target < nums[mid]) {
+            }
+
+            else{
+                if(target >= nums[start] && target < nums[mid]){
                     end = mid - 1;
-                } else {
+                }
+                else{
                     start = mid + 1;
                 }
             }
         }
+        
         return false;
     }
 };
