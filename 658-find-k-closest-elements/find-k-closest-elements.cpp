cd32795117375d79da06714c76b784edc398f63a
Parth Sohal <sohalparth72@gmail.com>
2025-05-14 18:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 36.3 MB (58.70%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        for (int i = 0; i < arr.size(); i++) {
-            int diff = abs(x - arr[i]);
-            int idx = i;
-            for (int j = i + 1; j < arr.size(); j++) {
-                int dif = abs(x - arr[j]);
-                // cout << dif << " " ;
-                if (dif < diff) {
-                    // cout << dif << " " << arr[j] << dif << endl;
-                    idx = j;
-                    diff = dif;
-                }
+        int i = 0;
+        int j = arr.size()-1;
+
+        while ((j - i + 1) > k) {
+            int a = abs(arr[i] - x);
+            int b = abs(arr[j] - x);
+            if (a > b) {
+                i++;
+            } else {
+                j--;
             }
-            // cout << arr[idx] << endl;
-            swap(arr[i], arr[idx]);
         }
-        vector<int>result;
-        sort(arr.begin(), arr.begin() + k);
-        for(int i = 0 ; i < k ;i++){
-            result.push_back(arr[i]);
+
+        vector<int> result;
+
+        for (int k = i; k <= j; k++) {
+            result.push_back(arr[k]);
         }
+
         return result;
     }
 };
