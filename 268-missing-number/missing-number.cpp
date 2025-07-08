fef6056691ffed3b1e6ee933a7d90dec6a19d420
Parth Sohal <sohalparth72@gmail.com>
2025-07-08 21:00:04 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (38.61%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
 
-        sort(nums.begin() , nums.end()) ;
+        // sort(nums.begin() , nums.end()) ;
 
-        for(int i = 0 ; i < nums.size() ;i++){
-            if(nums[i] != i){
-                return i;
-            }
-        }
+        // for(int i = 0 ; i < nums.size() ;i++){
+        //     if(nums[i] != i){
+        //         return i;
+        //     }
+        // }
 
-        return nums.size();
+        // return nums.size();
+
+        int n = nums.size();
+        // cout << n << endl;
+        int total = ((n) * (n + 1)) / 2;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        return total - sum;
+
         // int xor1 = 0 ;
         // int xor2 = 0 ;
 
