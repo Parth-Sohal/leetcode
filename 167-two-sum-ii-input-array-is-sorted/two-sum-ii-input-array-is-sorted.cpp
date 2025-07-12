9de4b3e5210b79a911d4a28c82712e36d7a4a598
Parth Sohal <sohalparth72@gmail.com>
2025-07-12 21:39:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (31.96%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> ans ; 
-        
-        int i = 0 ;
-        int j = numbers.size()-1;
+        // vector<int> ans ;  
+        // int i = 0 ;
+        // int j = numbers.size()-1;
 
 
-        while(i < j ){
-            int sum = numbers[i] + numbers[j];
+        // while(i < j ){
+        //     int sum = numbers[i] + numbers[j];
 
-            if(sum == target){
-                return {i+1,j+1};
-            }
+        //     if(sum == target){
+        //         return {i+1,j+1};
+        //     }
 
-            else if( sum > target ){
-                j--;
-            }
-            else{
-                i++;
+        //     else if( sum > target ){
+        //         j--;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+
+        // return ans;
+
+        // binary search mathod
+
+        for(int i = 0 ; i < numbers.size() ;i++){
+            int j = i+1;
+            int k = numbers.size() - 1 ; 
+
+
+            // binarty search 
+            while(j <= k){
+
+                int mid = (j + k) / 2 ;
+
+                if(numbers[mid] == target - numbers[i]){
+                    return {i+1 , mid+1};
+                }
+
+                else if(numbers[mid] > target - numbers[i]){
+                    k = mid - 1;
+                }
+
+                else{
+                    j = mid + 1;
+                }
+
             }
         }
 
-        return ans;
+        return {};
 
     }
 };
