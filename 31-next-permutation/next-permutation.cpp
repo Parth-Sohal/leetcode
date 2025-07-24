708b61060646ccb857c45aa350929ed3289ca254
Parth Sohal <sohalparth72@gmail.com>
2025-07-25 02:14:07 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (92.35%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,25 +2,34 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
 
-        // in built -->
-        // next_permutation(nums.begin(),nums.end());
+        // better stl use kara hai bhaijan
+        // next_permutation(nums.begin() , nums.end()) ;
 
+        // optimisig try kari hai
 
-        // interveiw wala --
+        int n = nums.size();
+        int idx = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            // cout << i << " " << i+1 << endl;
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break;
+            }
+        }
 
-        // int n = nums.size();
-
-        // for(int i = n - 2 ; i >= 0 ; i--){
-        //     if(nums[i] < nums[i+1]){
-        //         reverse(nums.begin()+i+1,nums.end());
-        //         swap(nums[i],nums[i+1]);
-        //         return ;
-        //     }
-        // }
-
-        // return sort(nums.begin(),nums.end());
-
-
-        next_permutation(nums.begin(), nums.end());
+        if (idx == -1)  sort(nums.begin(), nums.end());
+    
+        else { // next greater element
+            int i = n - 1;
+            while (i > idx) {
+                if (nums[idx] == nums[i] || nums[i] < nums[idx]) {
+                    i--;
+                } else {
+                    swap(nums[i], nums[idx]);
+                    reverse(nums.begin() + idx + 1, nums.end());
+                    break;
+                }
+            }
+        }
     }
 };
