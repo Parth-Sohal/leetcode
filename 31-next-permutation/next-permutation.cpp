373208a7b1be3fc8477756157af6328951467177
Parth Sohal <sohalparth72@gmail.com>
2025-07-27 02:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.17%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
+    void nextPermutation(vector<int>& arr) {
 
-        // break point jaha longest prefix mile
-
-        int n = nums.size() ;
         int idx = -1;
+        int n = arr.size();
+
         for (int i = n - 2; i >= 0; i--) {
-            if (nums[i + 1] > nums[i]) {
+            // arr[i] > arr[i+1] se increasing me tha tooh ho raha tha mtlb koi
+            // bhi replace wala mill nahi raha tha
+            if (arr[i + 1] > arr[i]) {
                 idx = i;
                 break;
             }
         }
+        // cout << idx << endl;
 
-        if (idx == -1)
-            reverse(nums.begin(), nums.end());
+        if (idx == -1) {
+            reverse(arr.begin(), arr.end());
+        }
 
         else {
+            // just the next greater element ko replace karna hoga
+
             int i = n - 1;
-            while (i > idx && nums[i] <= nums[idx]) {
+            while (i > idx) {
+                if (arr[i] > arr[idx]) {
+                    swap(arr[idx], arr[i]);
+                    reverse(arr.begin() + idx + 1, arr.end());
+                    
+                    break;
+                }
                 i--;
             }
-
-            swap(nums[idx], nums[i]);
-            reverse(nums.begin() + idx + 1, nums.end());
         }
     }
 };
