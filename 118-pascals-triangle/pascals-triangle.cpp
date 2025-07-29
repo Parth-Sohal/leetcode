1fdd610700f34c43391741172ab155c07d60c573
Parth Sohal <sohalparth72@gmail.com>
2025-07-30 03:28:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (10.67%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,25 +1,55 @@
 class Solution {
 public:
+    int factorail(int n) {
+        if (n == 0)
+            return 1;
+        return n * factorail(n - 1);
+    }
+
+    int combination(int n, int r) {
+        return factorail(n) / (factorail(n - r) * factorail(r));
+    }
+
     vector<vector<int>> generate(int numRows) {
 
         vector<vector<int>> mat;
 
-        vector<int> ans;
-        for (int i = 0; i < numRows; i++) {
-            ans.push_back(1);
-            mat.push_back(ans);
-        }
-        for (int i = 2; i < mat.size(); i++) {
-            for (int j = 0; j < mat[i].size(); j++) {
-                if (j == 0 || j == i)
-                    continue;
+        // vector<int> ans;
+        // for (int i = 0; i < numRows; i++) {
+        //     ans.push_back(1);
+        //     mat.push_back(ans);
+        // }
+        // for (int i = 2; i < mat.size(); i++) {
+        //     for (int j = 0; j < mat[i].size(); j++) {
+        //         if (j == 0 || j == i)
+        //             continue;
 
-                else {
-                    mat[i][j] = mat[i - 1][j] + mat[i - 1][j - 1];
-                }
+        //         else {
+        //             mat[i][j] = mat[i - 1][j] + mat[i - 1][j - 1];
+        //         }
+        //     }
+        // }
+
+        // for (int i = 0; i < numRows ; i++) {
+        //     vector<int> ans;
+        //     for (int j = 0; j <= i; j++) {
+        //         ans.push_back(combination(i, j));
+        //     }
+        //     mat.push_back(ans);
+        // }
+
+
+        for(int i = 0 ; i < numRows ; i++){
+            int current = 1 ;
+            vector<int> row;
+            for(int j = 0 ; j <= i ; j++){
+                row.push_back(current);
+                current = current * (i - j) / ( j + 1);
             }
+            mat.push_back(row);
         }
 
-        return mat ;
+
+        return mat;
     }
 };
