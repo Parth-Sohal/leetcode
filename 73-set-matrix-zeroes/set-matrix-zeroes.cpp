37316452bb620f73691fe206972c403c2cbad247
Parth Sohal <sohalparth72@gmail.com>
2025-07-31 21:46:01 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (51.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,48 +1,53 @@
 class Solution {
 public:
-    vector<int> row(vector<vector<int>>& mat) {
-        vector<int> m;
-        for (int i = 0; i < mat.size(); i++) {
-            for (int j = 0; j < mat[i].size(); j++) {
-                if (mat[i][j] == 0) {
-                    m.push_back(i);
-                    break;
-                }
-            }
-        }
-        return m;
-    }
+    // vector<int> row(vector<vector<int>>& mat) {
+    //     vector<int> m;
+    //     for (int i = 0; i < mat.size(); i++) {
+    //         for (int j = 0; j < mat[i].size(); j++) {
+    //             if (mat[i][j] == 0) {
+    //                 m.push_back(i);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return m;
+    // }
 
-    vector<int> col(vector<vector<int>>& mat) {
-        vector<int> n;
-        for (int j = 0; j < mat[0].size(); j++) {
-            for (int i = 0; i < mat.size(); i++) {
-                if (mat[i][j] == 0) {
-                    n.push_back(j);
-                    break;
-                }
-            }
-        }
+    // vector<int> col(vector<vector<int>>& mat) {
+    //     vector<int> n;
+    //     for (int j = 0; j < mat[0].size(); j++) {
+    //         for (int i = 0; i < mat.size(); i++) {
+    //             if (mat[i][j] == 0) {
+    //                 n.push_back(j);
+    //                 break;
+    //             }
+    //         }
+    //     }
 
-        return n;
-    }
+    //     return n;
+    // }
 
     void setZeroes(vector<vector<int>>& matrix) {
 
-        vector<int> m = row(matrix);
-        vector<int> n = col(matrix);
-
         int row = matrix.size();
-        int col = matrix[0].size();
- 
+        int col = matrix[0].size() ;
 
-        for (int i = 0; i < m.size(); i++) {
-            matrix[m[i]] = vector<int>(col, 0);
+        vector<int> rowMat(row,0);
+        vector<int> colMat(col , 0);
+
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                if(matrix[i][j] == 0){
+                    rowMat[i] = 1;
+                    colMat[j] = 1;
+                }
+            }
         }
-
-        for (int i = 0; i < n.size(); ++i) {
-            for (int j = 0; j < row; j++) {
-                matrix[j][n[i]] = 0;
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                if(rowMat[i] || colMat[j]){
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
