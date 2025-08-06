a84343185d52aaa17bb7acefaa6d811147b16c19
Parth Sohal <sohalparth72@gmail.com>
2025-08-06 06:12:27 +0530

    Time: 38 ms (80.37%) | Memory: 45.4 MB (56.13%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,14 +4,36 @@ public:
         int count = 0 ;
         int n = nums.size();
 
+
+        // brtue tha 
+        // for(int i = 0 ; i < n ; i++){
+        //     int sum = 0 ;
+        //     for(int j = i ; j < n ; j++){
+        //         sum += nums[j] ;
+        //         if(sum == k)  count++;
+        //     }
+        // }
+
+
+        unordered_map<int,int> mpp;
+        int sum = 0 ;
+        mpp[0] = 1 ;
+
         for(int i = 0 ; i < n ; i++){
-            int sum = 0 ;
-            for(int j = i ; j < n ; j++){
-                sum += nums[j] ;
-                if(sum == k)  count++;
+
+            sum += nums[i];
+
+            int result = sum - k ;
+            if(mpp.find(result) != mpp.end()){
+                count+=mpp[result];
             }
+
+            mpp[sum]++;
         }
-    
+
+
+        
+        
         return count;
     }
 };
