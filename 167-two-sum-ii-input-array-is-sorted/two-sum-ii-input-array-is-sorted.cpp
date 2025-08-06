f9f1106bcd735c8149271b0db4354a4520486987
Parth Sohal <sohalparth72@gmail.com>
2025-08-07 03:18:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (63.62%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -25,32 +25,51 @@ public:
 
         // binary search mathod
 
-        for(int i = 0 ; i < numbers.size() ;i++){
-            int j = i+1;
-            int k = numbers.size() - 1 ; 
+        // for(int i = 0 ; i < numbers.size() ;i++){
+        //     int j = i+1;
+        //     int k = numbers.size() - 1 ; 
 
 
-            // binarty search 
-            while(j <= k){
+        //     // binarty search 
+        //     while(j <= k){
 
-                int mid = (j + k) / 2 ;
+        //         int mid = (j + k) / 2 ;
 
-                if(numbers[mid] == target - numbers[i]){
-                    return {i+1 , mid+1};
-                }
+        //         if(numbers[mid] == target - numbers[i]){
+        //             return {i+1 , mid+1};
+        //         }
 
-                else if(numbers[mid] > target - numbers[i]){
-                    k = mid - 1;
-                }
+        //         else if(numbers[mid] > target - numbers[i]){
+        //             k = mid - 1;
+        //         }
 
-                else{
-                    j = mid + 1;
-                }
+        //         else{
+        //             j = mid + 1;
+        //         }
 
+        //     }
+        // }
+
+        // return {};
+
+
+        int i = 0 ;
+        int j = numbers.size()-1;
+
+        while(i < j){
+            int sum = numbers[i] + numbers[j] ;
+
+            if(sum > target) j-- ;
+            else if(sum < target) i++;
+
+            else{
+                return {i+1,j+1};
             }
         }
 
         return {};
+
+
 
     }
 };
