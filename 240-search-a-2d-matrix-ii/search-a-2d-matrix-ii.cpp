325af5405427a7b90ee031ff88f1c645eb3e6c01
Parth Sohal <sohalparth72@gmail.com>
2025-08-10 01:34:13 +0530

    Time: 43 ms (90.12%) | Memory: 18.7 MB (67.57%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -5,51 +5,69 @@ public:
         int row = 0;
         int current = matrix[0].size() - 1;
 
-        int n = matrix.size()-1;
+        int n = matrix.size() - 1;
 
-        while(row <= n || current >= 0) {
+        // while(row <= n || current >= 0) {
+        //     // cout << matrix[row][current] << endl;
+        //     // cout << row << " " << current << endl;
+        //     if (matrix[row][current] == target) {
+        //         // cout << row << " " << current;
+        //         // cout << endl;
+        //         return true;
+        //     }
+
+        //     if (row == n && current == 0) {
+        //         // cout << "loop1" << endl;
+        //         // cout << "Nahi hai element guys " << endl;
+        //         break;
+        //     }
+
+        //     else if (row == n) {
+        //         // cout << "loop2" << endl;
+        //         // cout << "hey here " << endl;
+        //         if (matrix[row][current] < target) {
+        //             // cout << "no way" << endl;
+        //             break;
+        //         } else {
+        //             current--;
+        //         }
+        //     } else if (current == 0) {
+        //         // cout << "loop3" << endl;
+        //         if (matrix[row][current] > target) {
+        //             // cout << "no way" << endl;
+        //             break;
+        //         } else {
+        //             row++;
+        //         }
+        //     }
+
+        //     else if (matrix[row][current] < target) {
+        //         // cout << "loop4" << endl;
+        //         row++;
+        //     } else {
+        //         // cout << "loop5" << endl;
+        //         current--;
+        //     }
+        // }
+
+        // return false;
+
+
+        while(row <= n && current >= 0){
             // cout << matrix[row][current] << endl;
-            // cout << row << " " << current << endl;
-            if (matrix[row][current] == target) {
-                // cout << row << " " << current;
-                // cout << endl;
+            if(matrix[row][current] == target){
+                // cout << row << " " << current << endl;
                 return true;
             }
-
-            if (row == n && current == 0) {
-                // cout << "loop1" << endl;
-                // cout << "Nahi hai element guys " << endl;
-                break;
-            }
-
-            else if (row == n) {
-                // cout << "loop2" << endl;
-                // cout << "hey here " << endl;
-                if (matrix[row][current] < target) {
-                    // cout << "no way" << endl;
-                    break;
-                } else {
-                    current--;
-                }
-            } else if (current == 0) {
-                // cout << "loop3" << endl;
-                if (matrix[row][current] > target) {
-                    // cout << "no way" << endl;
-                    break;
-                } else {
-                    row++;
-                }
-            }
-
-            else if (matrix[row][current] < target) {
-                // cout << "loop4" << endl;
+            if(matrix[row][current] < target){
                 row++;
-            } else {
-                // cout << "loop5" << endl;
+            }else{
                 current--;
             }
         }
 
         return false;
+
+
     }
 };
