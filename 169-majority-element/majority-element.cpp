68e63f3128676dc1c9a79ba33e5aafa2737c48eb
Parth Sohal <sohalparth72@gmail.com>
2025-08-12 01:18:58 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (28.27%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -5,16 +5,30 @@ public:
         int current = nums[0];
         int count = 1 ;
 
-        for(int i = 1 ; i < nums.size() ;i++){
+        // for(int i = 1 ; i < nums.size() ;i++){
+        //     if(count == 0){
+        //         current = nums[i];
+        //         count = 1 ;
+        //     }
+        //     else if(nums[i] == current) count++;
+        //     else count--;
+        // }
+
+        // return current;
+
+        for(int i = 1 ; i < nums.size() ; i++){
             if(count == 0){
                 current = nums[i];
-                count = 1 ;
+                count =  1;
             }
-            else if(nums[i] == current) count++;
-            else count--;
+            else if(nums[i] == current){
+                count++;
+            }else{
+                count--;
+            }
         }
 
-        return current;
+        return current ;
 
     }
 };
