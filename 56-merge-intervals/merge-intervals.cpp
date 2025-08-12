08a790681a88362fcba3b0e2fc609eea33adfdf0
Parth Sohal <sohalparth72@gmail.com>
2025-08-12 09:56:01 +0530

    Time: 5 ms (55.54%) | Memory: 24 MB (31.39%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,38 +3,31 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
 
         sort(intervals.begin(), intervals.end());
+
         int start = intervals[0][0];
         int end = intervals[0][1];
 
-        int row = intervals.size();
-
-        int j = 1;
+        int j = 1 ;
 
         vector<vector<int>> ans;
 
-        while (j < row) {
 
-            if (intervals[j][0] <= end) {
-                if(intervals[j][1] > end){
-                    end = intervals[j][1];
-                }
-                // end = intervals[j][1];
+        while(j < intervals.size()){
+            if(intervals[j][0] <= end){
+                end = max(end, intervals[j][1]);
             }
-
-            else {
-                // cout << start << " " << end << endl;
-                ans.push_back({start, end});
+            else{
+                ans.push_back({start,end}) ;
                 start = intervals[j][0];
                 end = intervals[j][1];
-                // cout << "new start and end => ";
-                // cout << start << " " << end << endl;
             }
-
             j++;
         }
 
-        ans.push_back({start, end});
+
+        ans.push_back({start,end});
 
         return ans;
+        
     }
 };
