3b930fbafd0b34f08db877072a8cb6d5fd7cf3da
Parth Sohal <sohalparth72@gmail.com>
2025-08-15 21:06:17 +0530

    Time: 2 ms (55.13%) | Memory: 8 MB (3.31%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -4,21 +4,22 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-       int low = 1;
-       int high = n;
-       int ans = -1;
 
-       while(low <= high){
-        int mid = low + (high - low )/2;
-        int val = isBadVersion(mid);
-        if(val == 0){
-            low = mid + 1;
+        int start = 1 ;
+        int end = n ;
+        int ans = -1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(isBadVersion(mid)){
+                ans = mid ; 
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
         }
-        else{
-            ans = mid;
-            high = mid - 1;
-        }
-       } 
-       return ans;
+
+        return ans;
+        
     }
 };
