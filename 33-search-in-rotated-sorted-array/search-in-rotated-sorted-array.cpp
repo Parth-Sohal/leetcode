71f3220d341c9cfb3e5aa419b8977f50cdfd30e9
Parth Sohal <sohalparth72@gmail.com>
2025-08-16 20:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.03%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,53 +1,75 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums  , int start , int end ,int target){
-        while(start <= end){
-            int mid = (start+end)/2;
-            if(nums[mid] == target){
-                return mid ;
-            }else if(nums[mid] > target){
-                end = mid - 1;
-            }else{
-                start = mid + 1;
-            }
-        }
-        return -1;
-    }
-        
-    int search(vector<int>& nums, int target) {
-        
-        int start = 0 , end = nums.size() - 1;
+
+    int searchNoDublicates(vector<int>& arr,int target){
+
+        int start = 0 ;
+        int end = arr.size() - 1;
 
         while(start <= end){
 
-            int mid = (start + end) / 2;
+            int mid = (start + end)/2;
 
-            if(nums[mid] == target){
-                return mid ; 
-            }
+            if(arr[mid] == target)return mid;
 
-            else if(nums[mid] > nums[end]){
-                if(target >= nums[start] && target < nums[mid]){
+            else if(arr[mid] >= arr[start]){ // the sorted part is left
+                if(target >= arr[start] && target < arr[mid]){
                     end = mid - 1;
-                } else{
+                }else{
                     start = mid + 1;
                 }
             }
-
             else{
-                if(target > nums[mid] && target <= nums[end]){
-                    start = mid + 1 ;
+                if(target > arr[mid] && target <= arr[end]){
+                    start = mid + 1;
                 }else{
                     end = mid - 1;
                 }
-                
             }
 
-            // return -1;
-            
-        }
+        }   
 
         return -1;
+
+    }
+
+
+
+        
+    int search(vector<int>& nums, int target) {
+        
+        // int start = 0 , end = nums.size() - 1;
+
+        // while(start <= end){
+
+        //     int mid = (start + end) / 2;
+
+        //     if(nums[mid] == target){
+        //         return mid ; 
+        //     }
+
+        //     else if(nums[mid] > nums[end]){
+        //         if(target >= nums[start] && target < nums[mid]){
+        //             end = mid - 1;
+        //         } else{
+        //             start = mid + 1;
+        //         }
+        //     }
+
+        //     else{
+        //         if(target > nums[mid] && target <= nums[end]){
+        //             start = mid + 1 ;
+        //         }else{
+        //             end = mid - 1;
+        //         }
+                
+        //     }
+
+        //     // return -1;
+            
+        // }
+
+        return searchNoDublicates(nums,target);
 
         
     
