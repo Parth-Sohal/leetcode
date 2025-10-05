784ce95c123412eab560337bc459064b5e96439a
Parth Sohal <sohalparth72@gmail.com>
2025-10-05 13:42:33 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (51.94%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,26 +3,23 @@ class Solution {
 
         int low = 0 ;
         int high = nums.length - 1;
-        int ans = nums.length ;
+        int ans = high + 1;
 
-        while(low <= high){
-            int mid = (low + high )/ 2;
+        while( low <= high ){
+            int mid = (low + high )/ 2; 
 
+            if(nums[mid] == target) return mid ;
 
-            if(nums[mid] == target){
-                return mid ;
-            }
-        
-            else if(nums[mid] > target){
+            if(nums[mid] > target){
                 ans = mid ;
                 high = mid - 1;
-            }
-            else{
-                low = mid + 1;
+            }else{
+                low = mid + 1 ;
             }
 
         }
-        return ans   ;
-        
+
+        return ans;
+
     }
 }
