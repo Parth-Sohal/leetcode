a31faba3db95df02bf9ba5e676cb1f13c509749c
Parth Sohal <sohalparth72@gmail.com>
2025-11-01 23:49:38 +0530

    Time: 1 ms (17.88%) | Memory: 42.9 MB (5.43%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder builder = new StringBuilder(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 65 && s.charAt(i) <= 90) {
+                builder.append((char) (s.charAt(i) - 'A' + 97));
+            } else {
+                builder.append(s.charAt(i));
+            }
+            //           System.out.println(s.charAt(i) + " " + ((char)(s.charAt(i) - 'A' + 97)));
+        }
+
+        return builder.toString();
     }
 }
