29bde4bbd5ff9b76adbd77c894e2ac8c1e4ac6e3
Parth Sohal <sohalparth72@gmail.com>
2026-02-02 01:34:33 +0530

    Time: 6 ms (24.80%) | Memory: 53.4 MB (8.52%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -1,19 +1,15 @@
 class Solution {
 
     public List<String> letterCasePermutation(String s , int idx , String ans , List<String> list) {
-
-
+                                                                
         if (idx == s.length()) {
-            // System.out.println(ans);
             list.add(new String(ans));
             return list;
         }
 
         if((s.charAt(idx) >= 'a' && s.charAt(idx) <= 'z') || (s.charAt(idx) >= 'A' && s.charAt(idx) <= 'Z'))  {
             letterCasePermutation(s, idx+1, ans+(s.charAt(idx)+"").toLowerCase() , list);
-
             letterCasePermutation(s, idx+1, ans+(s.charAt(idx)+"").toUpperCase() ,list);
-
         }
 
         else{
@@ -21,8 +17,6 @@ class Solution {
         }
 
         return list ;
-
-
 
     }
 
