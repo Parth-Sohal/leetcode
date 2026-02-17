9eead11f763fe8572514cb1a0eec5ff698bc80e9
Parth Sohal <sohalparth72@gmail.com>
2026-02-17 22:38:21 +0530

    Time: 5 ms (83.63%) | Memory: 49 MB (55.26%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -1,56 +1,33 @@
 class Solution {
 
-    public List<String> binaryWatch(int[] curr , int idx , int hour , int min , int currCount , int n , int[] arr , List<String> lists){
+     public  String format(int h , int m){
 
-        if(hour > 11 || min > 59){
-            return lists;
+        String min = "";
+        if( m < 10 ){
+            min = "0" + m;
+        }else{
+            min = ""+m;
         }
 
-        if(currCount == n){
-//            System.out.println("min : " + min + " " + " hour : " + hour);
-             String minS = "";
-            if(min < 10){
-                minS = "0" + min;
-            }else{
-                minS = ""+min;
-            }
-
-            String s = hour + ":" + minS;
-            lists.add(s);
-            return lists ;
-        }
-
-
-
-        for(int i = idx ; i < curr.length; i++){
-
-            curr[i] = 1 ;
-
-            if(i <= 3){
-                binaryWatch(curr,i+1,hour + arr[i],min,currCount+1,n,arr,lists);
-            }
-
-            else{
-                binaryWatch(curr,i+1,hour,min+arr[i],currCount+1,n,arr,lists);
-            }
-
-            curr[i] = 0 ; // backtrack
-        }
-        
-        return lists;   
-
-
-
-
-
+        return h + ":" + min;
     }
-
-
 
     public List<String> readBinaryWatch(int turnedOn) {
-        int[] arr = {8, 4, 2, 1, 32, 16, 8, 4, 2, 1};
-        int[] build = new int[arr.length];
 
-        return binaryWatch(build,0,0,0,0,turnedOn,arr,new ArrayList<>());
+        List<String> ans = new ArrayList<>();
+
+        for(int i = 0 ; i < 12 ; i++){
+            for(int j = 0 ; j < 60 ; j++){
+                if(Integer.bitCount(i) + Integer.bitCount(j) == turnedOn){
+                    ans.add(format(i,j));
+                }
+            }
+        }
+
+        return ans ; 
+
     }
+
+
+
 }
