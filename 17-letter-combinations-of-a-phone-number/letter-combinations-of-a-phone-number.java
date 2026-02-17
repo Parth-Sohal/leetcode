07390b986727d50652e623532ea307901992f46b
Parth Sohal <sohalparth72@gmail.com>
2026-02-17 21:08:27 +0530

    Time: 3 ms (33.34%) | Memory: 49.2 MB (15.25%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,40 +1,38 @@
 class Solution {
 
-    public  List<String> phoneNumber(int idx ,String digit , String ans , HashMap<Integer,String> mpp ,List<String> list){
+    public List<String> letterCombination(String digit , int i , HashMap<Character, String> map , String ans , List<String> lists){
 
 
-        if(idx == digit.length()){
 
-            list.add(new String(ans));
+        if( i == digit.length()){
             // System.out.println(ans);
-
-            return list;
+            lists.add(ans);
+            return lists;
         }
 
-        int ch = digit.charAt(idx) - '0';
-        String s = mpp.get(ch);
-        for(int i = 0; i < s.length() ; i++){
-            phoneNumber(idx+1 , digit , ans+s.charAt(i) , mpp ,list);
+        String s = map.get(digit.charAt(i));
+        for(int idx = 0 ; idx < s.length() ;idx++){
+            letterCombination(digit,i+1,map,ans+s.charAt(idx) ,lists);
         }
 
+        return lists;
 
-        return list;
 
     }
 
     public List<String> letterCombinations(String digits) {
-        HashMap<Integer,String> map = new HashMap<>();
 
-        map.put(2,"abc");
-        map.put(3,"def");
-        map.put(4,"ghi");
-        map.put(5,"jkl");
-        map.put(6,"mno");
-        map.put(7,"pqrs");
-        map.put(8,"tuv");
-        map.put(9,"wxyz");
+        HashMap<Character, String> map = new HashMap<>();
 
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
 
-        return (phoneNumber(0,digits,"",map ,new ArrayList<>()));
+        return  letterCombination(digits , 0,map,"" , new ArrayList<>());
     }
 }
