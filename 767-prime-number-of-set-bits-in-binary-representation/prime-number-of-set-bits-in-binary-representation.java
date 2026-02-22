9872c46e16794569ee2f089df004cbdffadbacc9
Parth Sohal <sohalparth72@gmail.com>
2026-02-22 22:14:29 +0530

    Time: 4 ms (87.36%) | Memory: 42.4 MB (64.06%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,50 +1,13 @@
 class Solution {
-
-
-    public void primeNumbers(int[] arr){
-
-        arr[0] = -1;
-        arr[1] = -1;
-
-        for(int i = 2 ; i*i < arr.length ; i++){
-
-            if(arr[i] == -1)continue;
-
-            else{
-
-                for(int j = (i*i) ; j < arr.length ; j += i){
-                    arr[j]  = -1;
-                }
-
-            }
-
-
-        }
-
+    public int countPrimeSetBits(int L, int R) {
+        int ans = 0;
+        for (int x = L; x <= R; ++x)
+            if (isSmallPrime(Integer.bitCount(x)))
+                ans++;
+        return ans;
     }
-     public int countPrimeSetBits(int left, int right , int[] arr) {
-
-        int count = 0 ;
-        for(int i = left ; i <= right ; i++){
-
-            int n = Integer.bitCount(i);
-
-            if(arr[n] == 0) {
-                count++;
-            }
-
-        }
-
-        return count ;
-
-    }
-
-    public int countPrimeSetBits(int left, int right) {
-        int[] arr = new int[32];
-
-        primeNumbers(arr);
-
-        return countPrimeSetBits(left,right,arr);
-
+    public boolean isSmallPrime(int x) {
+        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+                x == 11 || x == 13 || x == 17 || x == 19);
     }
 }
