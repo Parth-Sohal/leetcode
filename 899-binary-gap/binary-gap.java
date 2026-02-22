d44c7b71be1cda6b3b4f5da95b0a52ca90860d33
Parth Sohal <sohalparth72@gmail.com>
2026-02-22 21:25:00 +0530

    Time: 10 ms (3.15%) | Memory: 42.9 MB (14.06%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int binaryGap(int n) {
+
+        int count = 0;
+        int max = 0;
+        int last = -1;
+
+        while (n > 0) {
+
+            int currDigi = (n & 1);
+
+            if (currDigi == 1) {
+                System.out.println(count + " " + last);
+                if (last != -1)
+                    max = Math.max(max, count - last);
+                last = count;
+            }
+
+            count++;
+            n = n >> 1;
+
+        }
+
+        return max;
+
+    }
+}
