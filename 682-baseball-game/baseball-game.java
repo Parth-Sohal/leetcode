0004110d95191940a9a7e9864a3e24cd88fd5baf
Parth Sohal <sohalparth72@gmail.com>
2026-02-24 22:00:52 +0530

    Time: 171 ms (5.17%) | Memory: 47.1 MB (6.77%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int calPoints(String[] operations) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String s : operations) {
+
+            switch (s) {
+                case "+" -> {
+                    int a = stack.pop();
+                    int b = stack.peek();
+
+                    stack.push(a);
+                    stack.push(a + b);
+                }
+                case "C" -> stack.pop();
+                case "D" -> stack.push(stack.peek() * 2);
+                default -> {
+                    int num = Integer.parseInt(s);
+                    stack.push(num);
+                }
+            }
+
+            System.out.println(stack);
+
+        }
+
+        int sum = 0;
+
+        System.out.println(stack);
+
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+            //            System.out.print(stack.pop() + " ");
+        }
+
+        return sum ; 
+
+    }
+}
