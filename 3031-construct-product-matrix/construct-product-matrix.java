f75394be33e1771ef8703be63de337d88c369287
Parth Sohal <sohalparth72@gmail.com>
2026-03-24 11:03:45 +0530

    Time: 10 ms (84.31%) | Memory: 101.2 MB (31.86%) - LeetSync

---
+++ 3031-construct-product-matrix/construct-product-matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] constructProductMatrix(int[][] grid) {
+
+        int[][] prfMat = new int[grid.length][grid[0].length];
+
+
+        for(int i = 0;  i < grid.length ; i++){
+            for(int j = 0 ; j < grid[0].length ; j++){
+                grid[i][j] = grid[i][j]%12345;
+            }
+        }
+
+        int prd = 1;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                prfMat[i][j] = prd % 12345;
+                prd = (prd * grid[i][j]) % 12345;
+            }
+        }
+
+        prd = 1;
+        for (int i = grid.length - 1; i >= 0; i--) {
+            for (int j = grid[i].length - 1; j >= 0; j--) {
+                prfMat[i][j] = (prd * prfMat[i][j]) % 12345 ;
+                prd = (prd * grid[i][j]) % 12345;
+            }
+        }
+
+        return prfMat ;
+    }
+}
