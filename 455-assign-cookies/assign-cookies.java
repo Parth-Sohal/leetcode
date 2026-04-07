c099df81570d6ac8f4cc0c4d244905410ce84f3a
Parth Sohal <sohalparth72@gmail.com>
2026-04-08 01:42:26 +0530

    Time: 14 ms (94.25%) | Memory: 51.3 MB (79.77%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int total = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if(s[j] >= g[i]){
+                total++;
+                i++;
+            }
+
+            j++;
+        }
+
+        return total ; 
+
+    }
+}
