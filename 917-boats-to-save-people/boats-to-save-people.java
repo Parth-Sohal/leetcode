28139e4c7a41530f1746dad7b34698638517a9f2
Parth Sohal <sohalparth72@gmail.com>
2026-04-08 00:43:39 +0530

    Time: 20 ms (83.23%) | Memory: 56.2 MB (91.78%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        Arrays.sort(people) ;
+
+        int i = 0 ;
+        int j = people.length - 1;
+        int totalBoats = 0;
+
+        while(i <= j){
+
+            if(people[i] + people[j] <= limit){
+                i++;
+            }
+
+            j--;
+
+            totalBoats++;
+        }
+
+        return totalBoats ;
+
+    
+
+
+    }
+}
