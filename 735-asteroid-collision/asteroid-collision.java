3898ce45db84790ba8ea8e2bfd6f1ebc48b7af03
Parth Sohal <sohalparth72@gmail.com>
2026-04-11 02:00:25 +0530

    Time: 9 ms (5.35%) | Memory: 47.5 MB (5.97%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] asteroidCollision(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i : arr) {
+
+            if (i >= 0) {
+                st.push(i);
+            }
+
+            else {
+
+                // last neg or empty direct push
+
+                while (!st.isEmpty() && st.peek() >= 0 && st.peek() < Math.abs(i)) {
+                    st.pop();
+                }
+
+                if (!st.isEmpty() && st.peek() == Math.abs(i)) {
+                    st.pop();
+                    continue;
+                }
+
+            
+                if (!st.isEmpty() && st.peek() >= 0 && st.peek() > Math.abs(i)) {
+                    continue;
+                } else {
+                    st.push(i);
+                }
+
+            }
+            // System.out.println(st);
+
+        }
+
+return st.stream().mapToInt(Integer::intValue).toArray();
+        
+    }
+}
