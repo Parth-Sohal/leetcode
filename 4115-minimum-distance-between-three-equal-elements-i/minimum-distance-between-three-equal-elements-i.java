3fbca064f4eed1b9399da6896546cf00c1fecece
Parth Sohal <sohalparth72@gmail.com>
2026-04-10 12:38:31 +0530

    Time: 3 ms (82.64%) | Memory: 45.4 MB (10.93%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -1,31 +1,41 @@
 class Solution {
     public int minimumDistance(int[] arr) {
-        int n = arr.length ;
-        if(n < 3)return -1 ; 
+
+        HashMap<Integer, ArrayList<Integer>> mpp = new HashMap<>();
+
+        for (int idx = 0; idx < arr.length; idx++) {
+
+            if (mpp.containsKey(arr[idx])) {
+                mpp.get(arr[idx]).add(idx);
+            } else {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(idx);
+                mpp.put(arr[idx], list);
+            }
+
+        }
+
+        // System.out.println(mpp);
 
         int minDist = Integer.MAX_VALUE;
-        
-        
-        for(int i = 0 ; i < arr.length ; i++){
-            int val = arr[i]; 
-            
-            for(int j = i+1 ; j < arr.length ; j++){
-                if(arr[j] == val){
-                    for(int k = j+1 ; k < arr.length ; k++){
-                        if(arr[k] == val){
-                            int dist = Math.abs(j-k) + Math.abs(i-k) + Math.abs(j-i) ;
-                            minDist = Math.min(minDist,dist);
-                            break ;
-                        }
+
+        for (Integer key : mpp.keySet()) {
+
+            if (mpp.get(key).size() >= 3) {
+                ArrayList<Integer> indices = mpp.get(key);
+                if (indices.size() >= 3) {
+                    for (int idx = 0; idx <= indices.size() - 3; idx++) {
+                        int i = indices.get(idx);
+                        int k = indices.get(idx + 2); // The third element in this triplet
+
+                        int dist = 2 * (k - i);
+                        minDist = Math.min(minDist, dist);
                     }
                 }
             }
-            
+
         }
 
-        return minDist == Integer.MAX_VALUE ? -1 : minDist ; 
-
-
-
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
     }
 }
