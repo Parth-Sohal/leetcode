839d255182d25d312f676d8546cce03374453a2a
Parth Sohal <sohalparth72@gmail.com>
2026-04-17 03:58:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (45.52%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,25 +1,19 @@
 class Solution {
     public int reverseBits(int n) {
-        
-        int count = 0;
-        int ans = 0;
 
-        while (count <= 31) {
 
-            ans = ans << 1;
+        int ans = 0 ;
 
-            if ((n & 1) == 1) {
-                ans = ans | 1;
-            }
+        for(int i = 0 ; i < 32 ; i++){
+            ans = ans << 1 ;
 
-            n = n >> 1;
+            ans = ans | ( n & 1);
 
-            System.out.println("n = " + n + " ans = " + ans);
+            n = n >>> 1 ;
 
-            count++;
-
-        }
+        }        
 
         return ans ; 
+        
     }
 }
