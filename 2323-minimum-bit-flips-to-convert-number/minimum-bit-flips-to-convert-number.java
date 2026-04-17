82f768b0fd5315f4938b2e845d7e084e2c74886a
Parth Sohal <sohalparth72@gmail.com>
2026-04-17 16:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (22.02%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,21 +1,22 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int count = 0;
-        while ((start > 0) || (goal > 0)) {
 
-            // System.out.println(start + " " + goal);
 
-            int a = start & 1;
-            int b = goal & 1;
+        int count = 0 ;
 
-            if ((a ^ b) == 1) {
-                // System.out.println("a ->" + a + " : b-> " + b);
-                count++;
-            }
+        while(start != 0 || goal != 0){
 
-            start = start >> 1;
-            goal = goal >> 1;
+            int a = start & 1 ;
+            int b = goal & 1 ;
+
+            if(a!=b)count++;
+
+            start = start >> 1; 
+            goal = goal >> 1; 
+
+
         }
+        
         return count ; 
     }
 }
