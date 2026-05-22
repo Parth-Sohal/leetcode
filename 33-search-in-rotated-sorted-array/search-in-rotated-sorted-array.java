3f215ba36dbd8f4d01142ec2289294f9ae653e99
Parth Sohal <sohalparth72@gmail.com>
2026-05-22 12:41:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (45.24%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,34 +1,39 @@
 class Solution {
-    public int search(int[] nums, int target) {
-
+    public int search(int[] nums, int k) {
         int low = 0 ;
-        int high = nums.length - 1 ;
+        int high = nums.length -1 ;
 
-        while(low<=high){
-            int mid = (low+high)/2;
-            
-            if(nums[mid]==target){
+        while(low <= high){
+
+            int mid = low + (high - low)/2 ;
+
+            if(nums[mid] == k){
                 return mid;
             }
-            
-            if(nums[mid] > nums[high]){
-                if(nums[low] <= target && target < nums[mid]){
-                    high = mid-1;
-                }else{
-                    low = mid + 1;
-                }
-            }
-            
-            else{
-                if(nums[mid] < target && nums[high] >= target){
-                    low = mid + 1;
-                }else{
+
+            if(nums[mid] >= nums[low]) // left side sorted hai
+            {
+                if(k >= nums[low] && k < nums[mid]){
                     high = mid - 1;
+                }
+                else{
+                    low = mid + 1 ;
                 }
             }
 
+
+            else{ // right part sorted hai isme check kaar lo element ko
+
+
+                if(k > nums[mid] && k <= nums[high]){
+                    low  = mid + 1 ;
+                }else{
+                    high = mid - 1 ;
+                }
+
+            }
         }
-            
+
         return -1;
     }
 }
