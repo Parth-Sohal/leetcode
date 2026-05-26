5448528b9b2a3288e3402968ad10f4621124e70c
Parth Sohal <sohalparth72@gmail.com>
2026-05-26 17:56:46 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (71.43%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -1,37 +1,42 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        
 
-        int[] arr = new int[26] ;
+        int[] arr = new int[26];
 
+        int bit = 0;
 
-        for(int i = 0 ; i < word.length() ; i++){
+        for (int i = 0; i < word.length(); i++) {
 
             char ch = word.charAt(i);
 
-            if(ch >= 'A' && ch <= 'Z'){
-                arr[ch - 'A'] = 1 ; 
+            if (ch >= 'A' && ch <= 'Z') {
+                bit = bit | (1 << (ch - 'A' + 1));
             }
 
         }
 
-        int count = 0 ; 
+        int count = 0;
 
-        for(int i = 0 ; i < word.length() ; i++){
+        for (int i = 0; i < word.length(); i++) {
 
             char ch = word.charAt(i);
+            // int mask = 1 << (ch - 'a');
 
-            if(ch >= 'a' && ch >= 'Z'){
-                if(arr[ch -'a'] == 1)
-                {count++;
-                    arr[ch - 'a'] = 0 ; 
+            if (ch >= 'a' && ch <= 'z') {
+
+                int mask = 1 << (ch - 'a' + 1);
+
+                if ((bit & mask) != 0) {
+                    count++;
+
+                    // remove so duplicate lowercase doesn't count again
+                    bit = bit ^ mask;
                 }
             }
 
         }
 
-        return count ; 
-
+        return count;
 
     }
 }
