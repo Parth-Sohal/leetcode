acc1297cb13e162ed001480fc061e00779983199
Parth Sohal <sohalparth72@gmail.com>
2026-06-21 23:36:02 +0530

    Time: 36 ms (77.33%) | Memory: 84 MB (43.59%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        
+        Arrays.sort(costs);
+
+        int ans = 0 ; 
+
+        for(int i : costs){
+
+            if(i <= coins){
+                ans++;
+                coins -= i ;
+            }
+            else{
+                break ;
+            }
+            
+        }
+
+
+        return ans ; 
+
+        
+        
+    }
+}
