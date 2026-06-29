9c7f07354acc6a79db4a197369eb65f0d35c00ea
Parth Sohal <sohalparth72@gmail.com>
2026-06-30 03:44:38 +0530

    Time: 2 ms (60.28%) | Memory: 51.2 MB (28.63%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int helper(int[] nums, int goal) {
+
+        int total = 0 ;
+        int start = 0 ;
+        int sum = 0 ;
+
+        for(int i = 0 ; i < nums.length ; i++){
+
+            sum += nums[i];
+            
+            while(start <= i && sum > goal){
+                sum -= nums[start++];
+            }
+
+            total += (i - start + 1);
+            
+        }
+        
+        
+        return total;
+        
+    }
+
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+
+        return helper(nums , goal) - helper(nums , goal - 1) ; 
+
+
+
+    }
+}
