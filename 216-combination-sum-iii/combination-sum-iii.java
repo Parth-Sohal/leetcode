fd8898bae39f9aba9642b4438e0ceec83d16ce5d
Parth Sohal <sohalparth72@gmail.com>
2026-07-05 02:11:10 +0530

    Time: 5 ms (1.46%) | Memory: 43 MB (13.34%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,29 +1,29 @@
 class Solution {
-    public static List<List<Integer>> sum3( int idx , int n , int k , int sum , List<Integer> list , List<List<Integer>> lists ){
+
+    public  void combinationhelper3(int sum , int count ,  int idx ,  int k , int n , List<Integer> list , List<List<Integer>> lists){
 
 
-        if(sum>n)return lists;
-
-        if(list.size() == k){
-//            System.out.println(list);
-            if(sum == n){
-                lists.add(new ArrayList<>(list));
-//                System.out.println("Real ans : " + list);
-            }
-            return lists;
+        if(list.size() == k && sum == n){
+            System.out.println(list + " " + sum);
+            lists.add(new ArrayList<>(list));
+            return;
         }
 
+        if(sum >= n || count > k){
+            return ;
+        }
 
-        for(int i = idx ; i <= n && i <= 9 ; i++){
+        for(int i = idx ; i <= 9 ; i++){
             list.add(i);
-            sum3(i+1 , n , k , sum + i ,list ,lists );
-            list.remove(list.size() - 1);
+            combinationhelper3(sum+i , count+1 , i+1 , k , n , list ,lists);
+            list.removeLast();
         }
 
+   }
 
-        return lists ;
-    }
     public List<List<Integer>> combinationSum3(int k, int n) {
-        return sum3(1,n,k,0,new ArrayList<>() , new ArrayList<>()) ;
+        List<List<Integer>> ans =  new ArrayList<>() ;
+        combinationhelper3(0,0, 1 , k , n, new ArrayList<>() , ans);
+        return ans ; 
     }
 }
