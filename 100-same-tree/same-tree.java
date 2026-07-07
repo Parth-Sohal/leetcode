fdadf202ece3d714ee88e74f74eaa2533e0e5b87
Parth Sohal <sohalparth72@gmail.com>
2026-07-07 11:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (15.09%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,23 +15,63 @@
  */
 class Solution {
 
-    public boolean isSameTree2(TreeNode p, TreeNode q) {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
 
         if(p == null || q == null){
             return p == q ; 
-        }
+        } 
         
-        if(p.val != q.val){
-            return false;
-        }
-        
-        return isSameTree2(p.left, q.left) && isSameTree2(p.right, q.right);
-        
-        
-        
-    }
+        Queue<TreeNode> q1 = new LinkedList<>() ;
+        Queue<TreeNode> q2 = new LinkedList<>() ;
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        return isSameTree2(p,q);
+        q1.offer(p);
+        q2.offer(q);        
+
+        while(!q1.isEmpty() && !q2.isEmpty()){
+
+            if(q1.size()  != q2.size()){
+                return false ; 
+            }
+
+            int s = q1.size() ;
+
+            for(int i = 0 ; i < s ; i++){
+                TreeNode temp1 = q1.poll() ;
+                TreeNode temp2 = q2.poll() ;
+
+                if(temp1.val != temp2.val)return false ; 
+
+                if(temp1.left == null || temp2.left == null){
+                    if(temp1.left != temp2.left)return false ;
+                }
+                else{
+                    if(temp1.left != null){
+                        q1.add(temp1.left);
+                    }
+                    if(temp2.left != null){
+                        q2.add(temp2.left) ;
+                    }
+                }
+
+
+                if(temp1.right == null || temp2.right == null){
+                    if(temp1.right != temp2.right)return false ;
+                }
+                else{
+                    if(temp1.right != null){
+                        q1.add(temp1.right);
+                    }
+                    if(temp2.right != null){
+                        q2.add(temp2.right) ;
+                    }
+                }
+
+            }
+
+        }
+
+        return true ; 
+
+
     }
 }
