814a7a6ac80a61c8e567bc02fa27840c9973893a
Parth Sohal <sohalparth72@gmail.com>
2026-07-07 09:44:01 +0530

    Time: 1 ms (99.85%) | Memory: 42.8 MB (34.98%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,36 +1,31 @@
 class Solution {
-
-    long reverse(long rev , long n){
-
-        if(n == 0){
-            return rev ; 
-        }
-
-        if(n % 10 == 0){
-            return reverse(rev , n / 10) ;
-        }
-
-        rev = (rev * 10) + (n % 10) ;
-
-        return reverse(rev , n / 10) ;
-
-    }
-
-    long sum(long n){
-
-        if(n == 0)return 0 ;
-
-        return n%10 + sum(n / 10);
-    }
-
-
     public long sumAndMultiply(int n) {
+
+        long ans = 0 ;
+        long sum = 0 ;
+        int place = 1; 
         
-        long n2 = reverse(0L, (long)n) ;
-        long sum = sum(n2);
+
+        while(n > 0){
+
+            int digit = n % 10 ; 
+
+            if(digit % 10 != 0){
+
+                ans = digit * place + ans ; 
+                place = place * 10 ; 
+
+            }
+
+            n = n / 10 ;
+            sum += digit ; 
+
+        }
+
+        return ans * sum ; 
 
 
-        return reverse(0,n2) * sum ;
 
+        
     }
 }
