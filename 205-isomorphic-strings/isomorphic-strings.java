784c92b7e9524b51d84559f61e819a9232840790
Parth Sohal <sohalparth72@gmail.com>
2026-07-08 16:33:40 +0530

    Time: 24 ms (12.43%) | Memory: 44.2 MB (16.18%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,29 +1,44 @@
 class Solution {
+
+    public static boolean isomorphic(String a , String b){
+
+            // e g g // a d d
+
+
+        // e - a
+       // g - d
+
+       if(a.length()!=b.length()){
+           return  false;
+       }
+
+       int n = a.length();
+       HashMap<Character,Character> mapAtoB = new HashMap<>();
+       HashMap<Character,Character> mapBtoA = new HashMap<>();
+
+       for(int i=0;i < n ;i++){
+
+           char c1 =  a.charAt(i) ;
+           char c2 =  b.charAt(i) ;
+
+           if(mapAtoB.containsKey(c1) && mapAtoB.get(c1) != c2){
+               return false; 
+           }
+           
+           if(mapBtoA.containsKey(c2) && mapBtoA.get(c2) != c1) {
+               return false;
+           }
+           
+           mapAtoB.put(c1,c2);
+           mapBtoA.put(c2,c1);
+           
+       }
+
+       return true;
+       
+   }
+
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> mppStoT = new HashMap<>();
-        HashMap<Character,Character> mppTtoS = new HashMap<>();
-
-
-        for(int i = 0 ; i < s.length() ; i++){
-
-
-            char a = s.charAt(i);
-            char b = t.charAt(i);
-
-            if(mppStoT.containsKey(a) && mppStoT.get(a) != b){
-                return false ;
-            }
-            
-            if(mppTtoS.containsKey(b) && mppTtoS.get(b) != a){
-                return false ;
-            }
-
-            mppTtoS.put(b,a);
-            mppStoT.put(a,b);
-
-
-        }
-
-        return true ;
+        return isomorphic(s,t); 
     }
 }
