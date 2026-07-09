4dfb5c6dac335d4ee5f0591267a932f090f237f4
Parth Sohal <sohalparth72@gmail.com>
2026-07-09 11:17:51 +0530

    Time: 1 ms (72.33%) | Memory: 43.6 MB (51.06%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,64 +15,24 @@
  */
 class Solution {
 
-    public static List<Integer> rightView(TreeNode root) {
+    public static void rightVeiw(TreeNode root, List<Integer> list, int level) {
 
-        if(root == null){
-            return new ArrayList<>();
+        if (root == null) {
+            return;
         }
 
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.add(root);
-        List<Integer> list = new ArrayList<>();
-
-        while (!queue.isEmpty()) {
-
-            int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-
-                TreeNode temp = queue.poll();
-
-                if (i == size - 1) {
-                    list.add(temp.val);
-                }
-
-                if (temp.left != null) {
-                    queue.add(temp.left);
-                }
-
-                if (temp.right != null) {
-                    queue.add(temp.right);
-                }
-
-            }
-
-        }
-
-        return list ;
-
-    }
-
-
-    public static List<Integer> right(TreeNode root, List<Integer> list , int level){
-
-        if(root == null){
-            return list ;
-        }
-
-        if(level == list.size()){
+        if (level == list.size()) {
             list.add(root.val);
         }
 
-        right(root.right,list,level+1);
-        right(root.left,list,level+1);
-
-
-        return list ;
+        rightVeiw(root.right, list, level + 1);
+        rightVeiw(root.left, list, level + 1);
 
     }
 
     public List<Integer> rightSideView(TreeNode root) {
-        return right(root , new ArrayList<>() , 0); 
+        List<Integer> list = new ArrayList<>() ;
+        rightVeiw(root, list ,  0); 
+        return list ; 
     }
 }
