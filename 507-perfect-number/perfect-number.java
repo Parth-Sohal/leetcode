4b6353e5dcaed8c8d7b871aa4ed89db7b96208f8
Parth Sohal <sohalparth72@gmail.com>
2026-07-24 14:55:33 +0530

    Time: 2098 ms (34.00%) | Memory: 42.4 MB (10.22%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+
+        int sum = 0 ;
+
+        for(int i = 1 ; i < num ; i++){
+
+            if((num % i )== 0){
+                sum += i;
+            }
+
+        }
+        System.out.println(sum) ;
+
+        return sum == num ; 
+
+        
+    }
+}
