a6c50c080eb6f19abd0a652181c1fee4dd455515
Parth Sohal <sohalparth72@gmail.com>
2026-07-24 10:01:16 +0530

    Time: 8 ms (90.97%) | Memory: 46.8 MB (58.09%) - LeetSync

--- 2187-vowels-of-all-substrings/vowels-of-all-substrings.java
+++ 2187-vowels-of-all-substrings/vowels-of-all-substrings.java
@@ -1,27 +1,29 @@
-class Solution {
+class Solution { // contribution 
 
-    private static boolean isvowel(char c){
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')return true ;
 
+    // a b a
+
+    public boolean vowels(char c){
+        if(c == 'a' ||  c == 'e' ||  c == 'i' || c == 'o' || c == 'u')return true ;
         return false ; 
     }
 
     public long countVowels(String word) {
+        
+        long ans = 0 ;  
+        int n = word.length();
 
-        long ans = 0 ; 
-        int n = word.length() ;
-
-        for(int i = 0; i < word.length() ; i++){
-
-            char ch = word.charAt(i) ;
-
-            if(isvowel(ch)){
-                ans += (long)(i+1) * (n - i);
+        for(int i = 0 ; i < n ;i++){
+            char ch = word.charAt(i);
+            if(vowels(ch)){
+                ans += ((long)(i + 1) * (n - i));
             }
 
-        }
+        } 
+
 
         return ans ; 
-        
+
+
     }
 }
