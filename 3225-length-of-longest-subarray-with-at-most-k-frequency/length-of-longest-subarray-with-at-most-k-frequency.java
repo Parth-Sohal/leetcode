cd2110897345411476cec8b79a312f34f87a93bb
Parth Sohal <sohalparth72@gmail.com>
2026-08-12 09:37:57 +0530

    Time: 66 ms (74.64%) | Memory: 101.4 MB (22.01%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,31 +1,32 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        
-         int max = 0 ;
-        int start = 0 ;
-        
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for(int i = 0 ; i < nums.length ; i++){
-            
-            int n = nums[i];
-            
-            map.put(n, map.getOrDefault(n, 0)+1);
-            
-            while(map.get(n) > k){
-                
-                int num = nums[start] ;
-                map.put(num, map.get(num) - 1);
-                start++;
-                
-            }
-            
-            max = Math.max(max , i - start + 1);
-            
-            
-        }
-        
-        return max ; 
 
+
+        // the value of each element should come only till k 
+
+        int max = Integer.MIN_VALUE ;  
+        int start = 0 ; 
+        HashMap<Integer , Integer> map = new HashMap<>() ;
+
+        for(int i = 0 ; i < nums.length ; i++){
+
+            int val = nums[i] ;
+            map.put(val , map.getOrDefault(val,0) + 1);
+
+            while(map.get(val) > k){
+                
+                int numStart = nums[start];
+                map.put(numStart , map.get(numStart)  - 1) ;
+                start++ ;
+
+            }
+
+            max = Math.max(max , i - start + 1);
+
+
+        }
+
+        return max ; 
+        
     }
 }
