4e5d7a4497d9ca3ac092471cd43553a3e8297bbd
Parth Sohal <sohalparth72@gmail.com>
2026-08-30 17:08:56 +0530

    Time: 15 ms (47.06%) | Memory: 46.5 MB (64.08%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,31 +1,44 @@
 class Solution {
+
+    static class Pair{
+        Character ch ;
+        int count ;
+
+        Pair(Character ch,int count){
+            this.ch=ch;
+            this.count=count;
+        }
+
+    }
+
     public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
 
-        // System.out.println(map);
 
-        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        PriorityQueue<Pair> pq = new  PriorityQueue<>(
+                (a,b) -> b.count - a.count
+        );
 
-        pq.addAll(map.entrySet());
-
-        // System.out.println(pq);
-
-        StringBuffer sb = new StringBuffer();
-
-        while (!pq.isEmpty()) {
-            Map.Entry<Character, Integer> entry = pq.poll();
-            char c = entry.getKey();
-            int count = entry.getValue();
-
-            sb.append(String.valueOf(c).repeat(count));
+        for(Character ch : map.keySet()){
+            pq.offer(new Pair(ch,map.get(ch)));
         }
 
-        // System.out.println(sb.toString());
+        StringBuilder sb = new StringBuilder();
 
-        return sb.toString();
+        while(!pq.isEmpty()){
+            Pair p=pq.poll();
+
+            sb.repeat(p.ch ,p.count);
+
+        }
+
+        return (sb.toString());
+
+
     }
 }
